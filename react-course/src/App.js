f8b72f1a5d65135e60960031d6b6a219b7352699
creation00 <caraanjohncarlo@gmail.com>
2021-05-16 23:46:11 +0800

    today's progress

--- react-course/src/App.js
+++ react-course/src/App.js
@@ -1,14 +1,27 @@
-import Todo from './components/Todo'
+import { Route, Switch } from 'react-router-dom'
+
+import AllMeetupsPage from './pages/AllMeetups'
+import NewMeetupPage from './pages/NewMeetup'
+import FavoritesPage from './pages/Favorites'
+import Layout from './components/layout/Layout'
 
 const App = () => {
   return (
-    <div>
-      <h1>My Todos</h1>
-      <Todo title="HTML" />
-      <Todo title="CSS" />
-      <Todo title="JS" />
-      <Todo title="React" />
-    </div>
+    <Layout>
+      <Switch>
+        <Route path="/" exact>
+          <AllMeetupsPage />
+        </Route>
+
+        <Route path="/new-meetup">
+          <NewMeetupPage />
+        </Route>
+
+        <Route path="/favorites">
+          <FavoritesPage />
+        </Route>
+      </Switch>
+    </Layout>
   )
 }
 
