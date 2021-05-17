88670a08d397653a949f0366ab58cac3b5792c51
creation00 <caraanjohncarlo@gmail.com>
2021-05-17 19:08:49 +0800

    stuck at firebase fetch (will come back to this)

--- react-course/src/components/meetups/MeetupItem.js
+++ react-course/src/components/meetups/MeetupItem.js
@@ -1,17 +1,17 @@
 import Card from '../ui/Card'
 import classes from './MeetupItem.module.css'
 
-const MeetupItem = ({ meetup }) => {
+const MeetupItem = (props) => {
   return (
     <li className={classes.item}>
       <Card>
         <div className={classes.image}>
-          <img src={meetup.image} alt={meetup.title} />
+          <img src={props.meetup.image} alt={props.meetup.title} />
         </div>
         <div className={classes.content}>
-          <h3>{meetup.title}</h3>
-          <address>{meetup.address}</address>
-          <p>{meetup.description}</p>
+          <h3>{props.meetup.title}</h3>
+          <address>{props.meetup.address}</address>
+          <p>{props.meetup.description}</p>
         </div>
         <div className={classes.actions}>
           <button>To Favorites</button>
--- react-course/src/pages/NewMeetup.js
+++ react-course/src/pages/NewMeetup.js
@@ -1,7 +1,29 @@
 import React from 'react'
+import NewMeetupForm from '../components/meetups/NewMeetupForm'
 
 const NewMeetupPage = () => {
-  return <div>New Meetup Page</div>
+  const addMeetupHandler = (meetupData) => {
+    fetch(
+      'https://react-getting-started-dba01-default-rtdb.firebaseio.com/meetups',
+      {
+        method: 'POST',
+        body: JSON.stringify(meetupData),
+        headers: {
+          'Content-type': 'application/json',
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Methods': 'POST, GET, OPTIONS, DELETE, PUT',
+          'Access-Control-Allow-Headers':
+            'append,delete,entries,foreach,get,has,keys,set,values,Authorization',
+        },
+      }
+    )
+  }
+  return (
+    <section>
+      <h1>Add New Meetup</h1>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    </section>
+  )
 }
 
 export default NewMeetupPage
